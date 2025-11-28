79c0390ac7ad2b1e4e4b7c89849fe59d1456e419
d13g0h0h0 <diego.jamroz@gmail.com>
2025-11-28 01:31:29 +0100

    Implement MyExecService, add tests for all methods

--- src/main/java/uj/wmii/pwj/exec/MyExecService.java
+++ src/main/java/uj/wmii/pwj/exec/MyExecService.java
@@ -1,77 +1,284 @@
 package uj.wmii.pwj.exec;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class MyExecService implements ExecutorService {
 
+    private static class EndOfQueue<V> extends FutureTask<V> {
+        public EndOfQueue() {
+            super(() -> {}, null);
+        }
+    }
+
+    private final BlockingQueue<FutureTask<?>> queue = new LinkedBlockingQueue<>();
+    private boolean shutdown = false;
+    Thread thread = new Thread(() -> {
+        while(true) {
+            try {
+                FutureTask<?> task = queue.take();
+                if (task instanceof EndOfQueue<?>) {
+                    break;
+                }
+                else {
+                    task.run();
+                }
+            } catch (InterruptedException ignored) { }
+        }
+    });
+
+
+    private <T> boolean isCollectionNull(Collection<T> collection) {
+        for (T el : collection) {
+            if (el == null) return true;
+        }
+        return false;
+    }
+
+    private void cancelAll(Collection<? extends Future<?>> collection) {
+        for (Future<?> f : collection) {
+            f.cancel(true);
+        }
+    }
+
+    @SuppressWarnings("removal")
+    private void checkSecurityManager() {
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkPermission(new RuntimePermission("modifyThread"));
+        }
+    }
+
+    public MyExecService() {
+        thread.start();
+    }
+
     static MyExecService newInstance() {
-        return null;
+        return new MyExecService();
     }
 
     @Override
     public void shutdown() {
-
+        checkSecurityManager();
+        shutdown = true;
+        queue.add(new EndOfQueue<>());
     }
 
     @Override
     public List<Runnable> shutdownNow() {
-        return null;
+        checkSecurityManager();
+        thread.interrupt();
+        shutdown = true;
+        return queue.stream().map(ft -> (Runnable) ft).toList();
     }
 
     @Override
     public boolean isShutdown() {
-        return false;
+        return shutdown;
     }
 
     @Override
     public boolean isTerminated() {
-        return false;
+        return !thread.isAlive();
     }
 
     @Override
     public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-        return false;
+        long millis = unit.toMillis(timeout);
+        long nanos = unit.toNanos(timeout) % 1_000_000;
+        thread.join(millis, (int) nanos);
+        return isTerminated();
     }
 
     @Override
     public <T> Future<T> submit(Callable<T> task) {
-        return null;
+        if (task == null) {
+            throw new NullPointerException("The executor doesn't accept null tasks.");
+        }
+        if (shutdown) {
+            throw new RejectedExecutionException("The executor is shut down.");
+        }
+        FutureTask<T> ft = new FutureTask<>(task);
+        queue.add(ft);
+        return ft;
     }
 
     @Override
     public <T> Future<T> submit(Runnable task, T result) {
-        return null;
+        if (task == null) {
+            throw new NullPointerException("The executor doesn't accept null tasks.");
+        }
+        if (shutdown) {
+            throw new RejectedExecutionException("The executor is shut down.");
+        }
+        FutureTask<T> ft = new FutureTask<>(task, result);
+        queue.add(ft);
+        return ft;
     }
+
 
     @Override
     public Future<?> submit(Runnable task) {
-        return null;
+        if (task == null) {
+            throw new NullPointerException("The executor doesn't accept null tasks.");
+        }
+        if (shutdown) {
+            throw new RejectedExecutionException("The executor is shut down.");
+        }
+        FutureTask<?> ft = new FutureTask<>(task, null);
+        queue.add(ft);
+        return ft;
     }
 
     @Override
     public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
-        return null;
+        if (tasks == null || isCollectionNull(tasks)) {
+            throw new NullPointerException("The executor doesn't accept null tasks.");
+        }
+        if (shutdown) {
+            throw new RejectedExecutionException("The executor is shut down.");
+        }
+        List<Future<T>> list = new ArrayList<>();
+        for (Callable<T> task : tasks) {
+            FutureTask<T> ft = new FutureTask<>(task);
+            list.add(ft);
+            submit(ft);
+        }
+        for (Future<T> f : list) {
+            try {
+                f.get();
+            } catch (InterruptedException ie) {
+                cancelAll(list);
+                throw ie;
+            } catch (ExecutionException ignored) { }
+        }
+        return list;
     }
 
     @Override
     public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
-        return null;
+        if (tasks == null || isCollectionNull(tasks)) {
+            throw new NullPointerException("The executor doesn't accept null tasks.");
+        }
+        if (shutdown) {
+            throw new RejectedExecutionException("The executor is shut down.");
+        }
+
+        long timeoutNanos = unit.toNanos(timeout);
+        long start = System.nanoTime();
+
+        List<Future<T>> list = new ArrayList<>();
+        for (Callable<T> task : tasks) {
+            FutureTask<T> ft = new FutureTask<>(task);
+            list.add(ft);
+            submit(ft);
+        }
+        for (Future<T> f : list) {
+            if (!f.isDone()) {
+                long now = System.nanoTime();
+                long timeRemaining = timeoutNanos - (now - start);
+                if (timeRemaining < 0) {
+                    cancelAll(list);
+                    return list;
+                }
+                try {
+                    f.get(timeRemaining, TimeUnit.NANOSECONDS);
+                } catch (InterruptedException ie) {
+                    cancelAll(list);
+                    throw ie;
+                } catch (TimeoutException e) {
+                    cancelAll(list);
+                    return list;
+                } catch (ExecutionException ignored) { }
+            }
+        }
+        return list;
     }
 
     @Override
     public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-        return null;
+        if (tasks == null || isCollectionNull(tasks)) {
+            throw new NullPointerException("The executor doesn't accept null tasks.");
+        }
+        if (tasks.isEmpty()) {
+            throw new IllegalArgumentException("Tasks may not be empty.");
+        }
+        if (shutdown) {
+            throw new RejectedExecutionException("The executor is shut down.");
+        }
+        Throwable lastException = null;
+        List<Future<T>> list = new ArrayList<>();
+        for (Callable<T> task : tasks) {
+            FutureTask<T> ft = new FutureTask<>(task);
+            list.add(ft);
+            submit(ft);
+        }
+        try {
+            for (Future<T> f : list) {
+                try {
+                    return f.get();
+                } catch (ExecutionException ee) {
+                    lastException = ee.getCause();
+                }
+            }
+        } finally {
+            cancelAll(list);
+        }
+
+        throw new ExecutionException("No task completed successfully.", lastException);
     }
 
     @Override
     public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        return null;
+        if (tasks == null || isCollectionNull(tasks)) {
+            throw new NullPointerException("The executor doesn't accept null tasks.");
+        }
+        if (shutdown) {
+            throw new RejectedExecutionException("The executor is shut down.");
+        }
+
+        long timeoutNanos = unit.toNanos(timeout);
+        long start = System.nanoTime();
+
+        Throwable lastException = null;
+        List<Future<T>> list = new ArrayList<>();
+        for (Callable<T> task : tasks) {
+            FutureTask<T> ft = new FutureTask<>(task);
+            list.add(ft);
+            submit(ft);
+        }
+        try {
+            for (Future<T> f : list) {
+                if (!f.isDone()) {
+                    long now = System.nanoTime();
+                    long timeRemaining = timeoutNanos - (now - start);
+                    if (timeRemaining < 0) {
+                        throw new TimeoutException("No task completed in required time");
+                    }
+                    try {
+                        return f.get(timeRemaining, TimeUnit.NANOSECONDS);
+                    } catch (ExecutionException ee) {
+                        lastException = ee.getCause();
+                    }
+                }
+            }
+        } finally {
+            cancelAll(list);
+        }
+
+        throw new ExecutionException("No task completed successfully.", lastException);
     }
 
     @Override
     public void execute(Runnable command) {
-
+        if (command == null) {
+            throw new NullPointerException("The executor doesn't accept null tasks.");
+        }
+        if (shutdown) {
+            throw new RejectedExecutionException("The executor is shut down");
+        }
+        submit(command);
     }
 }
--- src/test/java/uj/wmii/pwj/exec/ExecServiceTest.java
+++ src/test/java/uj/wmii/pwj/exec/ExecServiceTest.java
@@ -2,10 +2,10 @@ package uj.wmii.pwj.exec;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.RejectedExecutionException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -46,7 +46,7 @@ public class ExecServiceTest {
         MyExecService s = MyExecService.newInstance();
         StringCallable c = new StringCallable("X", 10);
         Future<String> f = s.submit(c);
-        doSleep(20);
+        doSleep(30);
         assertTrue(f.isDone());
         assertEquals("X", f.get());
     }
@@ -62,12 +62,169 @@ public class ExecServiceTest {
             () -> s.submit(new TestRunnable()));
     }
 
-    static void doSleep(int milis) {
+    @Test
+    void testInvokeAll() throws InterruptedException, ExecutionException {
+        int max = 4;
+        Integer[] answers = new Integer[max];
+        for (int i = 1; i <= max; i++) {
+            answers[i - 1] = i * i;
+        }
+        ExecutorService s = MyExecService.newInstance();
+        List<Callable<Integer>> list = getListCallableInteger(max);
+        List<Future<Integer>> result = s.invokeAll(list);
+        int i = 0;
+        for (Future<Integer> f : result) {
+            assertEquals(answers[i], f.get());
+            i++;
+        }
+    }
+
+    @Test
+    void testInvokeAllTimeoutFail() throws InterruptedException {
+        int max = 4;
+        ExecutorService s = MyExecService.newInstance();
+        List<Callable<Integer>> list = getListCallableInteger(max);
+        long timeout = 10;
+        List<Future<Integer>> result = s.invokeAll(list, timeout, TimeUnit.MILLISECONDS);
+        assertEquals(max, result.size());
+        for (Future<Integer> f : result) {
+            assertTrue(f.isCancelled());
+        }
+    }
+
+    @Test
+    void testInvokeAllTimeoutSuccess() throws InterruptedException, ExecutionException {
+        int max = 4;
+        long timeout = 100;
+        Integer[] answers = new Integer[max];
+        for (int i = 1; i <= max; i++) {
+            answers[i - 1] = i * i;
+            timeout += 100 * i;
+        }
+        ExecutorService s = MyExecService.newInstance();
+        List<Callable<Integer>> list = getListCallableInteger(max);
+        List<Future<Integer>> result = s.invokeAll(list, timeout, TimeUnit.MILLISECONDS);
+        int i = 0;
+        for (Future<Integer> f : result) {
+            assertEquals(answers[i], f.get());
+            i++;
+        }
+    }
+
+    @Test
+    void testInvokeAny() throws InterruptedException, ExecutionException {
+        int max = 4;
+        Integer[] answers = new Integer[max];
+        for (int i = 1; i <= max; i++) {
+            answers[i - 1] = i * i;
+        }
+        ExecutorService s = MyExecService.newInstance();
+        List<Callable<Integer>> list = getListCallableInteger(max);
+        Integer result = s.invokeAny(list);
+        boolean check = false;
+        for (int i = 0; i < max; i++) {
+            if (result.equals(answers[i])) {
+                check = true;
+                break;
+            }
+        }
+        assertTrue(check);
+    }
+
+    @Test
+    void testInvokeAnyTimeoutFail() throws InterruptedException {
+        int max = 4;
+        ExecutorService s = MyExecService.newInstance();
+        List<Callable<Integer>> list = getListCallableInteger(max);
+        long timeout = 10;
+        assertThrows(TimeoutException.class, () -> s.invokeAny(list, timeout, TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    void testInvokeAnyTimeoutSuccess() throws InterruptedException, ExecutionException, TimeoutException {
+        int max = 4;
+        long timeout = 200;
+        Integer[] answers = new Integer[max];
+        for (int i = 1; i <= max; i++) {
+            answers[i - 1] = i * i;
+        }
+        ExecutorService s = MyExecService.newInstance();
+        List<Callable<Integer>> list = getListCallableInteger(max);
+        Integer result = s.invokeAny(list, timeout, TimeUnit.MILLISECONDS);
+        boolean check = false;
+        for (int i = 0; i < max; i++) {
+            if (result.equals(answers[i])) {
+                check = true;
+                break;
+            }
+        }
+        assertTrue(check);
+    }
+
+    @Test
+    void testInvokeNullTasks() {
+        ExecutorService s = MyExecService.newInstance();
+        List<Callable<Integer>> list = null;
+        long timeout = 1;
+        List<Callable<Integer>> finalList = list;
+        assertThrows(NullPointerException.class, () -> s.invokeAll(finalList));
+        assertThrows(NullPointerException.class, () -> s.invokeAll(finalList, timeout, TimeUnit.NANOSECONDS));
+        assertThrows(NullPointerException.class, () -> s.invokeAny(finalList));
+        assertThrows(NullPointerException.class, () -> s.invokeAny(finalList, timeout, TimeUnit.NANOSECONDS));
+        list = new ArrayList<>();
+        list.add(null);
+        List<Callable<Integer>> finalList2 = list;
+        assertThrows(NullPointerException.class, () -> s.invokeAll(finalList2));
+        assertThrows(NullPointerException.class, () -> s.invokeAll(finalList2, timeout, TimeUnit.NANOSECONDS));
+        assertThrows(NullPointerException.class, () -> s.invokeAny(finalList2));
+        assertThrows(NullPointerException.class, () -> s.invokeAny(finalList2, timeout, TimeUnit.NANOSECONDS));
+    }
+
+    @Test
+    void testShutdownNow() throws InterruptedException {
+        int max = 4;
+        ExecutorService s = MyExecService.newInstance();
+        List<Callable<Integer>> list = getListCallableInteger(max);
+        for (Callable<Integer> task : list) {
+            s.submit(task);
+        }
+        List<Runnable> result = s.shutdownNow();
+        assertEquals(max - 1, result.size());
+    }
+
+    @Test
+    void testAwaitTermination() throws InterruptedException {
+        ExecutorService s = MyExecService.newInstance();
+        long millis = 10;
+        long start = System.nanoTime();
+        assertFalse(s.awaitTermination(millis, TimeUnit.MILLISECONDS));
+        long end = System.nanoTime();
+        assertTrue(end - start >= 1_000_000 * millis);
+        int max = 4;
+        List<Callable<Integer>> list = getListCallableInteger(max);
+        for (Callable<Integer> task : list) {
+            s.submit(task);
+        }
+        s.shutdown();
+        assertFalse(s.awaitTermination(100, TimeUnit.MILLISECONDS));
+        assertTrue(s.awaitTermination((long) (max + 1) * max / 2 * 100, TimeUnit.MILLISECONDS));
+    }
+
+
+    static void doSleep(int millis) {
         try {
-            Thread.sleep(milis);
+            Thread.sleep(millis);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    static List<Callable<Integer>> getListCallableInteger(Integer max) {
+        List<Callable<Integer>> result = new ArrayList<>();
+        for (int i = 1; i <= max; i++) {
+            result.add(new IntegerCallable(i));
+        }
+        return result;
     }
 
 }
@@ -75,16 +232,16 @@ public class ExecServiceTest {
 class StringCallable implements Callable<String> {
 
     private final String result;
-    private final int milis;
+    private final int millis;
 
-    StringCallable(String result, int milis) {
+    StringCallable(String result, int millis) {
         this.result = result;
-        this.milis = milis;
+        this.millis = millis;
     }
 
     @Override
     public String call() throws Exception {
-        ExecServiceTest.doSleep(milis);
+        ExecServiceTest.doSleep(millis);
         return result;
     }
 }
@@ -94,5 +251,24 @@ class TestRunnable implements Runnable {
     @Override
     public void run() {
         wasRun = true;
+    }
+}
+
+
+
+class IntegerCallable implements Callable<Integer> {
+    Integer val;
+    public IntegerCallable(Integer val) {
+        this.val = val;
+    }
+
+    @Override
+    public Integer call() {
+        try {
+            Thread.sleep((long) 100 * val);
+            return val * val;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
